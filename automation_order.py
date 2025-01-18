170c0789e4f4766df9db98061c091f627b9286ec
satoshi25 <satoshi25.kr@gmail.com>
2025-01-18 20:47:09 +0900

    options 추가

--- automation_order.py
+++ automation_order.py
@@ -958,11 +958,11 @@ def add_order_sheet(df, order):
 def init_driver():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-software-rasterizer')
-    chrome_options.add_argument('--user-data-dir=/tmp/chrome-data')
+    chrome_options.add_argument('--user-data-dir=/home/chrome/chrome-data')
+    chrome_options.add_argument('--remote-debugging-port=9222') 
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
